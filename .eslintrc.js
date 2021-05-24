85831be63406eab7cc5e60467e4857d4e86db5c2
seungwoo Kim <kyh196201@gmail.com>
2021-05-25 00:51:41 +0900

    esLint, prettier 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,20 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 4,
+        trailingComma: "all",
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+        endOfLine: "auto",
+      },
+    ],
   },
   overrides: [
     {
