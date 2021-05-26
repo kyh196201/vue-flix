8bdeb4523773a66bdc4e78ebb8cb7d6092a6f414
seungwoo Kim <kyh196201@gmail.com>
2021-05-27 00:53:41 +0900

    scss 연동, scss boilerplate 폴더 구조 생성

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
@@ -22,5 +22,6 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+	background-color: $color-main;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ module.exports = {
 	devServer: {
 		overlay: false,
 	},
+
+	css: {
+		loaderOptions: {
+			scss: {
+				additionalData: `
+					@import "@/assets/scss/common.scss";
+					@import "@/assets/scss/variables.scss";
+				`,
+			},
+		},
+	},
 };
