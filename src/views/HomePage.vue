4ab61a911ece8adf0e26a392f3d24c1b74db6b4d
seungwoo Kim <kyh196201@gmail.com>
2021-06-15 00:02:09 +0900

    메인 페이지 빌보드 영역 마크업

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,18 +17,27 @@
 						<!-- <div class="billboard__trailer"></div> -->
 					</div>
 					<div class="billboard__front">
-						<!-- 영화 정보 -->
-						<div class="billboard__info">
-							<div class="billboard__title">너의 이름은</div>
-							<div class="billboard__overview">
-								새로운 교황 선출을 앞두고 일루미나티가 나타난다.
-								이들이 원하는 건 피의 복수뿐. 비극을 막으려면
-								단서를 찾아 해독해야 한다. 위험한 반물질에
-								바티칸이 파괴되기 전에.
+						<div class="billboard__front__inner">
+							<!-- 영화 정보 -->
+							<div class="billboard__info">
+								<div class="billboard__title">너의 이름은</div>
+								<div class="billboard__overview">
+									새로운 교황 선출을 앞두고 일루미나티가
+									나타난다. 이들이 원하는 건 피의 복수뿐.
+									비극을 막으려면 단서를 찾아 해독해야 한다.
+									위험한 반물질에 바티칸이 파괴되기 전에.
+								</div>
+							</div>
+							<!-- 비디오 버튼 및 연령 표시 -->
+							<div class="billboard__control">
+								<button type="button" class="btn btn--play">
+									재생
+								</button>
+								<button type="button" class="btn btn--info">
+									상세정보
+								</button>
 							</div>
 						</div>
-						<!-- 비디오 버튼 및 연령 표시 -->
-						<div class="billboard__control"></div>
 					</div>
 				</div>
 			</article>
