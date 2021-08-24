b202ffe3f56cdad9c18fc787aeb7e4de3b4b57f3
seungwoo Kim <kyh196201@gmail.com>
2021-08-25 01:03:53 +0900

    tmdb language 옵션 config 파일에 공통으로 설정

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,9 +1,13 @@
-// TMDB API KEY
+// tmdb api key
 export const API_KEY = process.env.VUE_APP_TMDB_API_KEY;
 
+// tmdb api domain
 export const TMDB_ENDPOINT = process.env.VUE_APP_TMDB_ENDPOINT;
 
+// tmdb api language
+export const TMDB_LANGUAGE = 'ko-KR';
+
 // TMDB Image base url
-// TODO '/configuration'을 통해서 기본 세팅하기
+// TODO: '/configuration'을 통해서 기본 세팅하기
 // TMDB API 이미지 base url
 export const baseImageUrl = 'https://image.tmdb.org/t/p/';
