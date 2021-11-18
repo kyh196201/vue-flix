6ff39ebd52e6d425a5cb8f33d3472aa61077d004
seungwoo Kim <kyh196201@gmail.com>
2021-11-19 01:44:56 +0900

    YoutubePlayer 소스 리팩토링

--- src/components/common/YoutubePlayer.vue
+++ src/components/common/YoutubePlayer.vue
@@ -59,7 +59,6 @@ export default {
 		const { videoId, width, height } = toRefs(props);
 		const playerVars = reactive({ ...toRefs(props.playerVars) });
 		const player = ref(null);
-		const playerState = ref(null);
 
 		const stateNames = {
 			[UNSTARTED]: 'unstarted',
@@ -70,17 +69,8 @@ export default {
 			[CUED]: 'video cued',
 		};
 
+		// Computed
 		const isMuted = computed(() => Boolean(playerVars.mute));
-
-		watch(isMuted, value => {
-			if (!player.value) return;
-
-			if (value) {
-				player.value.mute();
-			} else {
-				player.value.unMute();
-			}
-		});
 
 		// NOTE boolean -> 1, 0으로 어떻게 바꿨는지 참고하기
 		const getPlayerVars = function getPlayerVars(vars = {}) {
@@ -103,7 +93,46 @@ export default {
 			return resultVars;
 		};
 
-		const loadVideo = function loadVideo() {
+		const updatePlayer = function updatePlayer(newId) {
+			// load new video
+			console.log(newId);
+		};
+
+		// Event handlers
+		const handleStageChange = function handleStageChange(event) {
+			const state = stateNames[event.data];
+
+			if (!state) return;
+			context.emit('state-change', state);
+
+			if (event.data == ENDED) {
+				context.emit('end');
+			}
+		};
+
+		const handleReady = function handleReady(event) {
+			context.emit('ready', event.target);
+		};
+
+		const handleError = function handleError(event) {
+			context.emit('error', event);
+		};
+
+		// Watch
+		watch(isMuted, value => {
+			if (!player.value) return;
+
+			if (value) {
+				player.value.mute();
+			} else {
+				player.value.unMute();
+			}
+		});
+
+		watch(videoId, updatePlayer, { immediate: true });
+
+		// Hooks
+		onMounted(() => {
 			player.value = YouTubePlayer('player', {
 				height: height.value,
 				width: width.value,
@@ -115,38 +144,17 @@ export default {
 				player.value.playVideo();
 			}
 
-			player.value.on('stateChange', function ({ data }) {
-				const state = stateNames[data];
-
-				if (!state) return;
-
-				playerState.value = data;
-				context.emit('state-change', state);
-
-				if (data == 0 && !playerVars.loop) {
-					context.emit('end');
-				}
-			});
-		};
-
-		const initState = function initState() {
-			playerState.value = null;
-			player.value = null;
-		};
-
-		onMounted(() => {
-			loadVideo();
+			player.value.on('ready', handleReady);
+			player.value.on('stateChange', handleStageChange);
+			player.value.on('error', handleError);
 		});
 
 		onBeforeUnmount(() => {
-			initState();
+			if (player.value?.destroy) {
+				player.value.destroy();
+				player.value = null;
+			}
 		});
-
-		return {
-			player,
-			playerOptions: playerVars,
-			isMuted,
-		};
 	},
 };
 </script>
